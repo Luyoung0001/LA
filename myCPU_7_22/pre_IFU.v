9584ae37f6a3368075752d4b4d9b8d9e5f6f4b2e
Luyoung0001 <luyoung0001@gmail.com>
2025-07-23 17:15:03 +0800

    kind good

---
+++ myCPU_7_22/pre_IFU.v
@@ -0,0 +1,88 @@
+module pre_IFU (
+        input wire clk,             // 时钟信号
+        input wire rst,             // 复位信号
+        input wire [33:0] bus_br_data,
+        output wire [31:0] pc_o,
+
+        input wire [1:0] flush,
+        input wire [31:0] csr_era,
+        input wire [31:0] csr_eentry,
+        input wire [31:0] csr_tlbrentry,
+        input wbu_excp_tlbrefill,
+        // 握手信号
+        input wire waite_ready_i,
+        output wire state_valid,
+
+        // refetch
+        input wire [31:0] refetch_pc_i,
+        input wire refetch_sign_i,
+        input wire wbu_refetch_flush,
+
+        input wire icacop_flush_i
+    );
+
+    reg  [31:0] pc;
+    wire excp_flush;
+    wire ertn_flush;
+    wire flush_sign;
+    assign {excp_flush, ertn_flush} = flush;
+    assign flush_sign = ertn_flush || excp_flush || wbu_refetch_flush || icacop_flush_i;
+
+    wire [31:0] real_refetch_pc;
+    assign real_refetch_pc = icacop_flush_i ? refetch_pc_i + 32'd4 : refetch_pc_i;
+
+    wire [31:0] seq_pc;
+    wire [31:0] nextpc;
+
+    wire br_taken;
+    wire [31:0] br_target;
+    wire caculate_done;
+
+    wire [31:0] inst_flush_pc;
+
+    assign {br_taken, br_target,caculate_done} = bus_br_data;
+
+    assign inst_flush_pc = {32{ertn_flush}} & csr_era;
+    assign seq_pc       = pc + 32'h4;
+
+    // 这里要注意优先级
+    assign nextpc       =
+           refetch_sign_i || icacop_flush_i ? real_refetch_pc :
+           wbu_excp_tlbrefill ? csr_tlbrentry :
+           excp_flush ? csr_eentry:
+           ertn_flush ? inst_flush_pc:
+           br_taken ? br_target :
+           seq_pc;
+
+    reg [1:0] pfs_state;
+
+    always @(posedge clk) begin
+        if (rst) begin
+            pfs_state <= 2'd0;
+            pc <= 32'h1bfffffc;
+        end
+        else if(pfs_state == 2'd0 || flush_sign) begin
+            if(caculate_done && !flush_sign) begin
+                pfs_state <= 2'd1;
+                pc <= nextpc;
+            end
+            else if (flush_sign)begin
+                pfs_state <= 2'd1;
+                pc <= nextpc;
+            end
+
+        end
+        else if(pfs_state == 2'd1) begin
+            if(waite_ready_i) begin
+                pfs_state <= 2'd0;
+            end
+        end
+    end
+
+    assign pc_o = pc;
+    assign state_valid = (pfs_state == 2'd1)  ? 1'b1 : 1'b0;
+
+endmodule
+
+
+
